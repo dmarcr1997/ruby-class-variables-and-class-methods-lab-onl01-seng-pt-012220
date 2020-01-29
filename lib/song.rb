86b636fc6ca61f72d8e331bb1954f00060311753
Damon R <dmarcr1997@gmail.com>
2020-01-29 23:26:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,13 @@ class Song
   attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
+    @@genre << genre
+    @@artist << artist
     @name = name
     @artist = artist
     @genre = genre
     @@count+=1
-    @@genre << genre
-    @@artist << artist
+   
   end
   
   def self.genre
